a0608c7255cd1e31f07408e66c6bab550a570d31
SHARMILA JONES S <sharmilajones1011@gmail.com>
2026-03-08 17:34:18 +0530

    Enhance Rock Paper Scissors game with delays
    Refactor game logic to improve user experience and add delay for computer's choice.

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,59 +1,103 @@
 import random
+import time
+print("Welcome to Rock Paper Scissors!")
+print("rules:\n1)Rock beats Scissor\n2)Paper beats Rock\n3)Scissors beats Paper")
+print("Choose these keys to play\n 0 for rock\n1 for paper\n2 for scissors")
 def game():
     rounds=0;user_score=0;computer_score=0
-    print("Welcome to Rock Paper Scissors!")
-    print("rules:\n1)Rock beats Scissor\n2)Paper beats Rock\n3)Scissors beats Paper")
-    print("Choose these keys to play\n 0 for rock,1 for paper,2 for scissors")
-    while rounds <= 2:
+    r=0
+    while r<=1:
         computer = random.randint(0, 2)
-        num=input("enter your choice:")
-        if num==computer:
+        num=int(input("enter your choice:"))
+        if computer==num:
+            print("computer is thinking.....")
+            time.sleep(2)
             print("draw")
-            rounds+=1
-        if num==0:
-            if computer==2:
-                print("computer choice:", computer)
-                print("you won")
-                user_score+=1
-                rounds+=1
+            if computer == 0:
+                print("computer choice is Rock")
+            elif computer == 1:
+                print("computer choice is paper")
             else:
-                print("computer choice:", computer)
-                print("you lost")
-                computer_score+=1
-                rounds+=1
-        elif num==1:
-            if computer==0:
-                print("computer choice:", computer)
-                print("you won")
-                user_score+=1
-                rounds+=1
-            else:
-                print("computer choice:", computer)
-                print("you lost")
-                computer_score+=1
-                rounds+=1
+                print("computer choice is scissors")
+            r+=1
         else:
-            if computer==1:
-                print("computer choice:", computer)
-                print("you won")
-                user_score+=1
-                rounds+=1
+            if num==0:
+                if computer==2:
+                    print("computer is thinking.....")
+                    time.sleep(2)
+                    print("you won")
+                    if computer==0:
+                        print("computer choice is Rock")
+                    elif computer==1:
+                        print("computer choice is paper")
+                    else:
+                        print("computer choice is scissors")
+                    r += 1
+
+                else:
+                    print("computer is thinking.....")
+                    time.sleep(2)
+                    print("you lost")
+                    if computer==0:
+                        print("computer choice is Rock")
+                    elif computer==1:
+                        print("computer choice is paper")
+                    else:
+                        print("computer choice is scissors")
+                    r += 1
+            elif num==1:
+                if computer==0:
+                    print("computer is thinking.....")
+                    time.sleep(2)
+                    print("you won")
+                    if computer==0:
+                        print("computer choice is Rock")
+                    elif computer==1:
+                        print("computer choice is paper")
+                    else:
+                        print("computer choice is scissors")
+                    r += 1
+                else:
+                    print("computer is thinking.....")
+                    time.sleep(2)
+                    print("you lost")
+                    if computer==0:
+                        print("computer choice is Rock")
+                    elif computer==1:
+                        print("computer choice is paper")
+                    else:
+                        print("computer choice is scissors")
+                    r += 1
             else:
-                print("computer choice:", computer)
-                print("you lost")
-                computer_score+=1
-                rounds+=1
-        if rounds==3:
-            if user_score > computer_score:
-                print("you won this game")
-            elif user_score == computer_score:
-                print("draw match")
+                if computer==1:
+                    print("computer is thinking.....")
+                    time.sleep(2)
+                    print("you won")
+                    if computer==0:
+                        print("computer choice is Rock")
+                    elif computer==1:
+                        print("computer choice is paper")
+                    else:
+                        print("computer choice is scissors")
+                    r += 1
+                else:
+                    print("computer is thinking.....")
+                    time.sleep(2)
+                    print("you lost")
+                    if computer==0:
+                        print("computer choice is Rock")
+                    elif computer==1:
+                        print("computer choice is paper")
+                    else:
+                        print("computer choice is scissors")
+                    r += 1
+        again=input("Do you want to play again? \nsay y or n")
+        if again=="y" or "n":
+            if again=="y":
+                print("Choose these keys to play\n 0 for rock\n1 for paper\n2 for scissors")
+                print(game())
             else:
-                print("you lost better lusk next time")
-    print("\ncomputer_score:",computer_score,"\nuser_score:",user_score)
-    again=input("Do you want to play again? \nsay yes or no")
-    if again=="yes":
-        print(game())
-    else:
-        print("Thank you for playing")
+                print("Thank you for playing")
+                break
+        break
 print(game())
